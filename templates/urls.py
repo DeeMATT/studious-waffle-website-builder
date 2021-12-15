f551707684fe2d7a2dbc352d26fef37ba563e829
DeeMATT <mattode@outlook.com>
2021-12-15 11:56:37 +0100

    urls warning fix

--- templates/urls.py
+++ templates/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import upload_processed_template
 
 urlpatterns = [
-    path("/upload-template/", upload_processed_template)
+    path("upload-template/", upload_processed_template)
 ]
